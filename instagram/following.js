6d694c3431b7b965604e645daea312d45682b12c
nikitasenko <nikita.senko@mifort.org>
2018-10-10 16:53:59 +0300

    update

--- instagram/following.js
+++ instagram/following.js
@@ -5,7 +5,7 @@ Instagram = new Instagram();
 async function startFollow(acc_names) {
    await acc_names.forEach(async name => {
     const user = await Instagram.getUserDataByUsername(name);
-   await Instagram.follow(user.graphql.user.id,0);
+   await Instagram.follow(user.graphql.user.id,1);
   });
 
    setTimeout(async function f() {
@@ -20,8 +20,9 @@ async function start() {
   Instagram.getCsrfToken().then((csrf) =>
   {
     Instagram.csrfToken = csrf;
-    console.log('csrf' + csrf)
+    console.log('csrf ' + csrf)
   }).then(() => {
+      console.log(process.env.USER_NAME, process.env.USER_PASSWORD);
     return Instagram.auth(process.env.USER_NAME, process.env.USER_PASSWORD).then(async sessionId => {
       Instagram.sessionId = sessionId;
       console.log('sessionId ' + sessionId);
